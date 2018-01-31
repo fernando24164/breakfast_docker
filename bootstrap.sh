6e1588c34f325c34192dc7d22bdf318480c61c2d
fernando24164 <fernando24164@gmail.com>
2018-01-31 12:48:52 +0100

    Add docker-compose dependency and gitignore file

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,3 +27,6 @@ source ~/.profile
 
 docker pull ubuntu:14.04
 docker pull infoslack/dvwa
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
